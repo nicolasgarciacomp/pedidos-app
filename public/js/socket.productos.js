7958f82960e45e4b89b737552fdf71d01233a002
nicolasgarciacomp <nicolasgarciacomp@gmail.com>
2019-12-19 19:26:42 +0000

    Commit 19-12.2

--- public/js/socket.productos.js
+++ public/js/socket.productos.js
@@ -6,14 +6,16 @@ var tabProductos = $('#tabAllProductos');
 $(document).ready(function() {
 	socket.emit('getProductos', function(resp) {
 		for(var i = 0; i < resp.length; i++) {
-			tabProductos.append('<tr>');
-			tabProductos.append('<td>'+resp[i].id+'</td>');
-			tabProductos.append('<td>'+resp[i].tipo+'</td>');
-			tabProductos.append('<td>'+resp[i].nombre+'</td>');
-			tabProductos.append('<td>'+resp[i].precio+'</td>');
-			tabProductos.append('<td><a href="actualizar.html?id='+resp[i].id+'" class="btn btn-primary"><i class="fa fa-edit"></i></a></td>');
-			tabProductos.append('<td><a href="" data-id="'+resp[i].id+'" class="btn btn-danger"><i class="fa fa-trash-o"></i></a></td>');
-			tabProductos.append('</tr>');
+			var contenido = "";
+			contenido += '<tr>';
+			contenido += '<td>'+resp[i].id+'</td>';
+			contenido += '<td class="w50">'+resp[i].tipo+'</td>';
+			contenido += '<td class="w50">'+resp[i].nombre+'</td>';
+			contenido += '<td class="w50">'+resp[i].precio+'</td>';
+			contenido += '<td class="w50"><a href="actualizar.html?id='+resp[i].id+'" class="btn btn-primary"><i class="fa fa-edit"></i></a></td>';
+			contenido += '<td class="w50"><a href="" data-id="'+resp[i].id+'" class="btn btn-danger"><i class="fa fa-trash-o"></i></a></td>';
+			contenido += '</tr>';
+			tabProductos.append(contenido);
 		}
 	});
 });
