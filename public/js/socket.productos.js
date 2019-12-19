79755e572cfce3723f9763bad2462543db90e5bf
nicolasgarciacomp <nicolasgarciacomp@gmail.com>
2019-12-19 19:01:59 +0000

    Commit 19-12

--- public/js/socket.productos.js
+++ public/js/socket.productos.js
@@ -1,10 +1,19 @@
 // Comando para establecer la conexion
 var socket = io();
 
-var divProductos = $('#divAllProductos');
+var tabProductos = $('#tabAllProductos');
 
-$('button').on('click', '#btnAllProductos', function() {
+$(document).ready(function() {
 	socket.emit('getProductos', function(resp) {
-		divProductos.text(resp);
+		for(var i = 0; i < resp.length; i++) {
+			tabProductos.append('<tr>');
+			tabProductos.append('<td>'+resp[i].id+'</td>');
+			tabProductos.append('<td>'+resp[i].tipo+'</td>');
+			tabProductos.append('<td>'+resp[i].nombre+'</td>');
+			tabProductos.append('<td>'+resp[i].precio+'</td>');
+			tabProductos.append('<td><a href="actualizar.html?id='+resp[i].id+'" class="btn btn-primary"><i class="fa fa-edit"></i></a></td>');
+			tabProductos.append('<td><a href="" data-id="'+resp[i].id+'" class="btn btn-danger"><i class="fa fa-trash-o"></i></a></td>');
+			tabProductos.append('</tr>');
+		}
 	});
 });
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -4,21 +4,9 @@ const { Producto } = require('../classes/productos.js');
 const producto = new Producto();
 
 io.on('connection', (client) => {
-
-    client.on('siguienteTicket', (data, callback) => {
-        let siguiente = ticketControl.siguiente();
-        console.log(siguiente);
-        callback(siguiente);
-    });
-
-    client.emit('estadoActual', {
-    	actual: ticketControl.getUltimoTicket(),
-        ultimos4: ticketControl.getUltimos4()
-    });
     
     client.on('getProductos', (callback) => {
     	let getProductos = producto.getProductos();
-
     	callback(getProductos);
     });
 });
